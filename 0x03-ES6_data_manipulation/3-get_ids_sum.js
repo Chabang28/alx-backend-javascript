ad97e43c7cc36635d42b88b03c731e36de835668
Chabang28 <chabangmnisi@gmail.com>
2024-07-30 18:29:14 +0000

    reduce filters

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+export default function getStudentsIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
